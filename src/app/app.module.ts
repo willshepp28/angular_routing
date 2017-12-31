e6a7bf5bb33bb18a1bd325748f297e4b33395f5f
willshepp28 <willsheppard28@outlook.com>
2017-12-31 16:48:38 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user/user.component';
+
+// configure routes
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'user', children: [
+    { path: 'list', component: UserListComponent, children: [
+      { path: 'detail/:name', component: UserComponent }
+    ]}
+  ]},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+{ path: '**', redirectTo: '/home', pathMatch: 'full' }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    UserListComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+
+  users = [
+    { name: 'john'},
+    { name: 'peter'},
+    { name: 'sarah'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
